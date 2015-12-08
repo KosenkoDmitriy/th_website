cfb8ba996de8975ac48b089d85fa93e151953cce
dmitry <kosenkodmitryv@gmail.com>
2015-12-08 23:11:42 +0300

    refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def signup
     user = User.new(user_params)
     password = params['user']['password'] if params['user'].present? && params['user']['password'].present?
-    user.password = Digest::MD5.hexdigest(password)
+    user.password = pass(password)
     if user.save!
       # respond_with user
       render plain: "registered successfully! your email: #{ user.email }"
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     email = params['user']['email'] if params['user'].present? && params['user']['email'].present?
     password = params['user']['password'] if params['user'].present? && params['user']['password'].present?
     if email.present? && password.present?
-      password = Digest::MD5.hexdigest(password)
+      password = pass(password)
       if User.exists?(email: email, password: password)
         user = User.find_by(email: email, password: password)
         render plain: "#{user.email} #{user.phone_number}"

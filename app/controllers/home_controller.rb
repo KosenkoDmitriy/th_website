09018d268924759c1420111d08c8bbfee374caaa
dmitry <kosenkodmitryv@gmail.com>
2015-12-12 21:11:02 +0300

    Refactoring views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @user = User.new
+    @creditList = get_credits
   end
 
   def show
@@ -10,13 +11,19 @@ class HomeController < ApplicationController
 
   def buy
     # TODO: integrate with PayPal
+    @creditList = get_credits
+  end
+
+  private
+  def get_credits
     @credtits = Rails.configuration.x
-    @creditList = [
+    creditList = [
         {cost: @credtits.one.cost, value: @credtits.one.value},
         {cost: @credtits.two.cost, value: @credtits.two.value},
         {cost: @credtits.three.cost, value: @credtits.three.value},
         {cost: @credtits.four.cost, value: @credtits.four.value}
     ]
+    return creditList
   end
 
 end

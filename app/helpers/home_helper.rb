84c7ae35c42362536173a5f0c34e367a20e9ed6f
dmitry <kosenkodmitryv@gmail.com>
2015-12-15 18:50:27 +0300

    clickable links in the footer

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,16 @@
 module HomeHelper
+  def credits_url
+    url = root_path
+    user = current_user
+    if user
+      url = user_path(user)
+    else
+      url = root_path + "#credits"
+    end
+    return url
+  end
+
+  def sign_in_up_url
+    root_path + "#sign_in_up"
+  end
 end

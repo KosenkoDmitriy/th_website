c1e72aa4ce394ba19941710748fea28c09befb9e
dmitry <kosenkodmitryv@gmail.com>
2016-04-29 15:42:48 +0300

    some css changes for captcha

--- config/initializers/simple_captcha.rb
+++ config/initializers/simple_captcha.rb
@@ -1,10 +1,16 @@
 SimpleCaptcha.setup do |sc|
   # default: 100x28
-  # sc.image_size = '120x40'
+  sc.image_size = '220x35'
 
   # default: 5
   sc.length = 6
 
+  sc.image_style = 'th'
+  sc.add_image_style('th', [
+     "-background '#f7e397'",
+     "-fill 'darkred'",
+     "-border 0",
+     "-bordercolor '#E0E2E3'"])
   # default: simply_blue
   # possible values:
   # 'embosed_silver',
@@ -16,7 +22,7 @@ SimpleCaptcha.setup do |sc|
   # 'charcoal_grey',
   # 'almost_invisible'
   # 'random'
-  sc.image_style = 'simply_red'
+  sc.image_style = 'th'
 
   # default: low
   # possible values: 'low', 'medium', 'high', 'random'

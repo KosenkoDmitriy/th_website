dfedb9f7dc5854c2c06b37df7a56136424e59111
dmitry <kosenkodmitryv@gmail.com>
2016-06-23 09:16:47 +0300

    new values for buying credits

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Credit.find_or_create_by(cost_in_cents: 99, credits: 25000)
-Credit.find_or_create_by(cost_in_cents: 299, credits: 100000)
-Credit.find_or_create_by(cost_in_cents: 599, credits: 350000)
-Credit.find_or_create_by(cost_in_cents: 999, credits: 750000)
+Credit.find_or_create_by(cost_in_cents: 99, credits: 1000000)
+Credit.find_or_create_by(cost_in_cents: 299, credits: 2000000)
+Credit.find_or_create_by(cost_in_cents: 599, credits: 8000000)
+Credit.find_or_create_by(cost_in_cents: 999, credits: 10000000)
 
 webgl_text = "if any problems - reload page (support all browsers except internet explorer)"
 webplayer_text = "if any problems - reload page (support all browsers except google chrome)"

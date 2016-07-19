28127fa42742657bcdd7bb9a928d8d28ba2201c9
dmitry <kosenkodmitryv@gmail.com>
2016-07-19 16:05:09 +0300

    Added credits and orders

---
+++ app/admin/credit.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Credit do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :id, :title, :cost_in_cents, :credits, :dt, :text, :created_at, :updated_at
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+
+end
---
+++ app/admin/order.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Order do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :user_id, :credit_id, :ip, :express_token, :express_payer_id, :created_at, :updated_at, :dt, :status
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+
+end

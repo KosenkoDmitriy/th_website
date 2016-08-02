583172e7b739187b5ae98a3f45ebfff7a4b3f349
dmitry <kosenkodmitryv@gmail.com>
2016-08-02 21:59:55 +0300

    changed currency (removed $, credits instead of money)

--- public/video_poker_jack/js/CLang.js
+++ public/video_poker_jack/js/CLang.js
@@ -2,7 +2,7 @@ TEXT_GAMEOVER  = "GAME OVER";
 TEXT_PLAY      = "PLAY";
 TEXT_BET       = "BET";
 TEXT_WIN       = "WIN";
-TEXT_MONEY     = "MONEY";
+TEXT_MONEY     = "CREDITS";
 TEXT_DEAL      = "DEAL";
 TEXT_BET_ONE   = "BET ONE";
 TEXT_MAX_BET   = "BET MAX";
@@ -10,7 +10,7 @@ TEXT_RECHARGE  = "RECHARGE";
 TEXT_EXIT      = "EXIT";
 TEXT_DRAW      = "DRAW";
 TEXT_NO_WIN    = "NO WIN";
-TEXT_CURRENCY  = "$";
+TEXT_CURRENCY  = "";
 TEXT_COMBO     = new Array(
                             "Royal Flush",
                             "Straight Flush",

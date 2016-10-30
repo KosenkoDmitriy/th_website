37fa4f68282675f8724f72e3d8f561943a6622e7
dmitry <kosenkodmitryv@gmail.com>
2016-10-30 11:49:58 +0300

    player in top 10 in all games (message in text and html formats)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,9 +4,24 @@ class UserMailer < ApplicationMailer
   end
 
   def msg_as_html(email, subject, body)
-    mail(to: email,
-         body: body,
-         content_type: "text/html",
-         subject: subject)
+    mail(to: email, subject: subject, body: body, content_type: "text/html")
+  end
+
+  def msg_as_text(email, subject, body)
+    mail(to: email, subject: subject, body: body)
+  end
+
+  def notify_user(user, msg)
+    @user=user
+    @msg=msg
+
+    # @top = 10
+    # days_in_month = Time.days_in_month(Time.now.month-1, Time.now.year) #.months_ago(1).month)
+    # dt_utc = DateTime.now.utc #.months_ago(1) prev month
+    # @dt1 = DateTime.new(dt_utc.year, dt_utc.month, 1).strftime("%Y-%m-%d")
+    # @dt2 = DateTime.new(dt_utc.year, dt_utc.month, days_in_month).strftime("%Y-%m-%d")
+    # @time_range = @dt1..@dt2
+
+    mail(to: user.email, subject: msg.title)
   end
 end

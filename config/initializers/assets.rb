f4663efbe001c595351894927e62ab1c933adf1a
dmitry <kosenkodmitryv@gmail.com>
2016-11-02 19:05:15 +0300

    production settings was changed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( mobile.css mobile.js )
+Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
+# Rails.application.config.assets.precompile += %w( *.js *.css )

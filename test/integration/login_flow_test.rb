e00efd41094cf54afb3a5992637443d604841d13
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-07-23 09:49:55 +0300

    Fixed ActiveRecord::Fixture::FixtureError: table "fw_histories" has no column named "float".

---
+++ test/integration/login_flow_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class HomePage < ActionDispatch::IntegrationTest
+  test "sign_in" do
+    # test "can see the sign in page" do
+    get "/sign_in"
+    text = "Please return to this site every 24 hours to spin the FREE credits wheel to get FREE credits each day."
+    # assert_select "h1", "users#index"
+    assert_select "h1", text
+  end
+  test "displays the user's name or email after sign in" do
+    email = "t@t.ru"
+    password = "pass"
+    # game = FactoryGirl.create!(:game, fid: 'texas_holdem_foldup')
+    user = User.create!(email: email, password: password)
+    get "/sign_in"
+    text = "Please return to this site every 24 hours to spin the FREE credits wheel to get FREE credits each day."
+    assert_select "h1", "users#index"
+    # assert_select "form.new_user" do
+    #   assert_select "input[name=?]", "email"
+    #   assert_select "input[name=?]", "password"
+    #   assert_select "input[type=?]", "submit"
+    # end
+    # post "/signin", email: email, password: password
+    # assert_select ".main_logo .text02 h4", :text => "Welcome {{email}}"
+
+  end
+end

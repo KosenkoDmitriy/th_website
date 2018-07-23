3b914d09f46c63b2eaca140b1cb095f56a1d249c
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-07-23 12:07:57 +0300

    NoMethodError: undefined method `visit' for #<RSpec::ExampleGroups::SignInUpPage::GETSignIn

--- spec/integration/login_flows_spec.rb
+++ spec/integration/login_flows_spec.rb
@@ -1,6 +1,12 @@
 # spec/integration == requests == api
 require 'rails_helper'
-
+FactoryGirl.define do
+  factory :user do
+    full_name             "Di Mi"
+    email                 "di@mi.ru"
+    password              "pass"
+  end
+end
 RSpec.describe "sign in/up page", type: :request do
   describe "GET /sign_in" do
     it "sign in page should work" do

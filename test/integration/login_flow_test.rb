b511fedb67be2226f7c47152674d8524eae89e34
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-07-23 15:11:31 +0300

    test welcome message after sign in

--- test/integration/login_flow_test.rb
+++ test/integration/login_flow_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'digest/md5'
 
 class HomePage < ActionDispatch::IntegrationTest
   test "can see the sign in page" do
@@ -8,15 +9,37 @@ class HomePage < ActionDispatch::IntegrationTest
     assert_select "h1", text
   end
   test "displays the user's name or email after sign in" do
-    email = "t@t.ru"
-    password = "pass"
-    user = User.create!(email: email, password: password)
-    assert_select "form.new_user" do
-      assert_select "input[name=?]", "email"
-      assert_select "input[name=?]", "password"
+    email = "s3@s.ru"
+    password = "s3@s.ru"
+
+    user = User.create!(email: email, password: Digest::MD5.hexdigest(password))
+
+    get sign_in_path
+    assert_response :success
+
+    assert_select "form.form-horizontal" do
+      # assert_select '#user_email[value=?]', user.email
+      # assert_select '#user_password[value=?]', user.password
+
+      assert_select "input[name=?]", "user[email]"
+      assert_select "input[name=?]", "user[password]"
+
+      # assert_select "input[name=?]", "user[email]" do
+      #   assert_select "[value=?]", email
+      # end
+      # assert_select "input[name=?]", "user[password]" do
+      #   assert_select "[value=?]", password
+      # end
       assert_select "input[type=?]", "submit"
     end
-    post "/signin", email: email, password: password
-    assert_select ".main_logo .text02 h4", :text => "Welcome {{email}}"
+    post_via_redirect "/signin", "user[email]": email, "user[password]": password
+    # post "/signin", user_email: email, user_password: password
+    # assert_response 302
+    # follow_redirect!
+    user.reload
+    assert_response :success
+    # get user_path user
+    assert_select ".text02", text: "Welcome #{email}"
+    # assert_select ".error", text: ""
   end
 end

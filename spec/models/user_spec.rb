dd39b28c88a3ca7967150738d69d9950c6508784
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-07-31 17:09:03 +0300

    Simplifying syntax for a spec of a user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,29 +2,33 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
 
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:user)).to be_valid
+    expect(build(:user)).to be_valid
+  end
+
   context "is valid" do
     it "with an email and password" do
-      user = User.new(email: 'test@test.com', password: 'test')
+      user = build(:user)
       expect(user).to be_valid
     end
   end
 
   context "is invalid" do
     it "without a password" do
-      user = User.new(email: 'test@test.com', password: nil)
+      user = build(:user, password: nil)
       user.valid?
       expect(user.errors[:password]).to include("can't be blank")
     end
     it "without an email" do
-      user = User.new(email: nil)
+      user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
     it "with a duplicate email" do
-      email = 'test@ex.ru'
-      password = 'test'
-      User.create(email: email, password: password)
-      user = User.new(email: email, password: password)
+      email = 'di@mi.ru'
+      create(:user, email: email)
+      user = build(:user, email: email)
       user.valid?
       expect(user.errors[:email]).to include('has already been taken')
     end
@@ -33,7 +37,7 @@ RSpec.describe User, type: :model do
   context "create" do
     it "with 2 or more orders" do
       email = "test@test.com"
-      user = User.create!(email: email, password: email)
+      user = create(:user, email: email, full_name: nil)
       expect(user.display_name).to eq(email)
 
       order = user.orders.create!(status: "paid")
